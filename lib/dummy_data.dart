fc9d990a635b5edca2f83e6ceb7b9e4d2bd17678
shaniX002 <malikshanilhr09@gmail.com>
2024-05-26 00:15:26 +0500

    Add new book

--- lib/dummy_data.dart
+++ lib/dummy_data.dart
@@ -1,6 +1,6 @@
 // lib/dummy_data.dart
-import 'package:oop/classwork/Book.dart' as lib1;
-import 'package:oop/classwork/book.dart' as lib2;
+import 'package:book/Book.dart' as lib1;
+import 'package:book/book.dart' as lib2;
 
 // Use the appropriate prefix when creating the dummyBooks list.
 final List<lib2.Book> dummyBooks = [
@@ -12,12 +12,25 @@ final List<lib2.Book> dummyBooks = [
   lib2.Book(
     title: '1984',
     author: 'George Orwell',
-    description: 'A dystopian social science fiction novel and cautionary tale.',
+    description:
+        'A dystopian social science fiction novel and cautionary tale.',
   ),
   lib2.Book(
     title: 'To Kill a Mockingbird',
     author: 'Harper Lee',
-    description: 'A novel about the serious issues of rape and racial inequality.',
+    description:
+        'A novel about the serious issues of rape and racial inequality.',
+  ),
+  lib2.Book(
+    title: 'The Great Gatsby',
+    author: 'F. Scott Fitzgerald',
+    description: 'A novel about the American dream.',
+  ),
+  lib2.Book(
+    title: 'Animal Farm',
+    author: 'George Orwell',
+    description:
+        'An allegorical novella reflecting events leading up to the Russian Revolution and then on into the Stalinist era of the Soviet Union.',
   ),
   // Add more books as needed
 ];
